29cf6b780492c301ec240b60fc26c28e05e39b0d
GiorgiGvasalia <ggvasalia4@gmail.com>
2024-07-24 18:46:39 +0400

    optimised image display

--- components/main-header.js
+++ components/main-header.js
@@ -1,12 +1,13 @@
 import styles from "./main-header.module.css";
 import Link from "next/link";
 import siteLogo from "@/assets/logo.png";
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
     <header className={styles.header}>
       <Link className={styles.logo} href="/">
-        <img src={siteLogo.src} alt="Site logo" />
+        <Image src={siteLogo} alt="Site logo" priority/>
         NEXTLEVEL FOOD
       </Link>
       <nav className={styles.nav}>
