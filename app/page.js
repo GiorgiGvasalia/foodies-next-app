1bd264f105709c361e67387c0417191e43237b64
GiorgiGvasalia <ggvasalia4@gmail.com>
2024-07-25 16:24:59 +0400

    added navigation && communty page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-"use client";
-
-import Link from "next/link";
 
 import classes from "./page.module.css";
 import ImageSlideshow from "@/components/images/image-slideshow";
@@ -16,10 +13,6 @@ export default function Home() {
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
             <p>Taste & share food from all over the world.</p>
-          </div>
-          <div className={classes.cta}>
-            <Link href="/community">Join the Community</Link>
-            <Link href="/meals">Explore Meals</Link>
           </div>
         </div>
       </header>
--- components/main-header.js
+++ components/main-header.js
@@ -2,21 +2,22 @@ import styles from "./main-header.module.css";
 import Link from "next/link";
 import siteLogo from "@/assets/logo.png";
 import Image from "next/image";
+import NavLink from "./nav-link/nav-link";
 
 export default function MainHeader() {
   return (
     <header className={styles.header}>
       <Link className={styles.logo} href="/">
-        <Image src={siteLogo} alt="Site logo" priority/>
+        <Image src={siteLogo} alt="Site logo" priority />
         NEXTLEVEL FOOD
       </Link>
       <nav className={styles.nav}>
         <ul>
           <li>
-            <Link href="/meals"> Browse Meals</Link>
+            <NavLink href="/meals">Browse Meals</NavLink>
           </li>
           <li>
-            <Link href="/community">Visit Community</Link>
+            <NavLink href="/community">Community</NavLink>
           </li>
         </ul>
       </nav>
