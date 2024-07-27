2503bc7408c3b02cc92819c2cd699be71b34e513
Giorgi Gvasalia <ggvasalia4@gmail.com>
2024-07-27 18:43:28 +0400

    added meals database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,22 +1,31 @@
 import Link from "next/link";
-import styles from './page.module.css'
+import styles from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
-    return (
-        <>
-            <header className={styles.header}>
-                <h1>Delicius meals, <span className={styles.highlight}> created by you</span></h1>
-            <p>Choose your favorite recipe and cook it yourseld, it is easy and fun.</p>
-            <p className={styles.cta}>
-                <Link href='/meals/share'>
-                    Share it
-                </Link>
-            </p>
-            </header>
-            <main className={styles.main}>
-                <MealsGrid meals={[]}/>
-            </main>
-        </>
-    )
+export default async function MealsPage() {
+  const meals = await getMeals();
+
+
+  
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicius meals,{" "}
+          <span className={styles.highlight}> created by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourseld, it is easy and fun.
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share it</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 }
